3621d307dbb470ed110c45a8b8ad9abb8625b47b
enyciaa <ian.andrew.alexander@gmail.com>
2014-06-08 21:20:56 +0800

    Obstacles half working
    added obstacles

--- helicopter.py
+++ helicopter.py
@@ -28,7 +28,7 @@ from kivy.properties import ListProperty, NumericProperty, \
 	ObjectProperty, BooleanProperty, ReferenceListProperty
 from kivy.vector import Vector
 from kivy.uix.popup import Popup
-from kivy.graphics import Bezier
+from random import randrange
 import random
 
 class StartPopUp(Popup):
@@ -130,32 +130,92 @@ class Tunnel(Widget):
         self.points_a=a_list
         self.points_b=b_list
 
-class Obstacles(Widget):
-    pass   
+class Obstacle(Widget):
+    'size of obstacle'
+    obstacle_height = NumericProperty(80)
+    obstacle_width = NumericProperty(30)
+    sizing = ReferenceListProperty(obstacle_width , obstacle_height)
+    
+    'obstacle speed'
+    velocity = ListProperty([-4, 0])
+    
+    'distance between obstacles'
+    distance = NumericProperty(250)
+    
+    #Bottom left corner of obstacle
+    pos_x = NumericProperty(0)
+    pos_y = NumericProperty(0)
+    position = ReferenceListProperty(pos_x , pos_y)
+    
+    #Has the next obstacle been addeed?
+    next_added = BooleanProperty(False)
+    
+    def __init__(self, HelicopterGame, **kw):
+        super(Obstacle, self).__init__(**kw)
+        self.HelicopterGame = HelicopterGame
+        self.start_position() 
+    
+    def start_position(self):
+        self.pos_x = self.HelicopterGame.get_right()  #x-co-ordinate of new obstacle
+        #need these to inherit from tunnel walls, self.HelicopterGame.tunnel.walls?
+        self.tunnel_top = 500
+        self.tunnel_bot = 100 - self.obstacle_height
+        self.pos_y = randrange(self.tunnel_bot, self.tunnel_top, 1)  #y-co-ordinate of new obstacle 
+    
+    #check if obstacle should be added
+    def add_check(self):
+        screen_right = self.HelicopterGame.get_right()
+        current_distance = screen_right - self.position[0]   #distance between right hand side of screen and current x co-ordinate of obstacle
+        cond1 = current_distance > self.distance
+        cond2 = self.next_added == False
+        if cond1 and cond2:
+            self.HelicopterGame.add_obstacle()
+            self.next_added = True
+            
+    #check if obstacle should be removed
+    def remove_check(self):
+        screen_left = 0 - self.obstacle_width  #x-coordinate of left side of screen minus obstacle width
+        if self.position[0] < screen_left:   #0 indicates first position in position list
+            self.HelicopterGame.remove_obstacle()
+
+    def update(self):
+        self.add_check()
+        self.remove_check()
+        self.position = Vector(*self.position) + Vector(*self.velocity)  #current position plus velocity
         
 class Helicopter(Widget):
-    #How to change these to percentages?
-    start_position = ListProperty([200, 200])
-    general_velocity = NumericProperty(3)
-    general_acceleration = NumericProperty(0.3)
-
-    velocity_x = NumericProperty(0); velocity_y = NumericProperty(0)
+    'Helicopter physics'
+    general_velocity = NumericProperty(2)
+    general_acceleration = NumericProperty(0.6)
+    
+    start_x = NumericProperty(0)
+    start_y = NumericProperty(0)
+    start_position = ReferenceListProperty(start_x, start_y)
+    
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
 
-    acceleration_x = NumericProperty(0); acceleration_y = NumericProperty(0)
+    acceleration_x = NumericProperty(0)
+    acceleration_y = NumericProperty(0)
     acceleration = ReferenceListProperty(acceleration_x, acceleration_y)
 
     touched_down = BooleanProperty(False)
     
     def __init__(self, **kw):
         super(Helicopter, self).__init__(**kw) 
+        self.initilise()
     
     #ensures helicopter isn't moving when game is restarted
+    #positions helicopter in start position
     def initilise(self):
         self.velocity_x = 0
         self.velocity_y = 0
         self.acceleration_x = 0
         self.acceleration_y = 0
+        #need these to be based on HelicopterGame widget
+        self.start_x = 200
+        self.start_y = 200
         self.pos = self.start_position
         
     def move(self):
@@ -181,9 +241,13 @@ class Helicopter(Widget):
             return True 
         
 class HelicopterGame(Widget):
+    'time until first obstacle'
+    time_first_ob = NumericProperty(2)
+    
     helicopter = ObjectProperty(None)
     background = ObjectProperty(None)
     tunnel = ObjectProperty(None)
+    obstacles = ListProperty([])
     game_state = BooleanProperty(False)
     
     def __init__(self, **kw):
@@ -196,14 +260,26 @@ class HelicopterGame(Widget):
     
     #Runs when popup is clicked        
     def start_game(self):
+        for obstacle in self.obstacles:
+            self.remove_widget(obstacle)
         self.helicopter.initilise()
-        self.game_state = True             
+        self.game_state = True  
+        Clock.schedule_once(self.add_obstacle, self.time_first_ob)   #adds obstacle after certain time        
                     
     def on_touch_down(self, touch):
         self.helicopter.touched_down = True
 
     def on_touch_up(self, touch):
         self.helicopter.touched_down = False 
+        
+    def add_obstacle(self, *args):
+        new_obstacle = Obstacle(self)  #passes helicopter game instance to obstacle widget
+        self.add_widget(new_obstacle)
+        self.obstacles = self.obstacles + [new_obstacle]   
+            
+    def remove_obstacle(self):
+        self.remove_widget(self.obstacles[0])
+        self.obstacles = self.obstacles[1:]    #not sure what this line does, moves next obstacle to start of array?
     
     #Runs when alive_check detects a collision    
     def end_game(self):
@@ -217,7 +293,9 @@ class HelicopterGame(Widget):
             self.helicopter.alive_check(self)     #passes helicoptergame instance to method
             self.background.scroll_background()
             self.helicopter.move()
-            self.tunnel.move()    
+            self.tunnel.move()  
+            for obstacle in self.obstacles:
+                obstacle.update()   
 
 class HelicopterApp(App):
     def build(self):
